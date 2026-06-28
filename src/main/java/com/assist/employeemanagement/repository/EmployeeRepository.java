e490e496e16616f15a297a9ec25222241e507384
shishupal lohani <shishupallohani@shishupals-MacBook-Air.local>
2026-06-28 11:03:19 +0530

    Initial commit

---
+++ src/main/java/com/assist/employeemanagement/repository/EmployeeRepository.java
@@ -0,0 +1,11 @@
+package com.assist.employeemanagement.repository;
+
+import com.assist.employeemanagement.model.Employee;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+
+@Repository
+public interface EmployeeRepository extends JpaRepository<Employee, Long>{
+
+}
